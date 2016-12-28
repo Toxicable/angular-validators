b8a31bc9b62f4ba00a072e3a9ef6494446d37d3c
Toxicable <fabianwiles@live.com>
2016-12-28 16:18:22 +1300

    Added Angular's Form Validator wrappers

--- spec/form-validators.spec.ts
+++ spec/form-validators.spec.ts
@@ -1,54 +1,172 @@
 import { FormValidators } from '../src/form-validators';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, FormArray, AbstractControl } from '@angular/forms';
 import { InvalidValidationResult } from '../src/invalid-validation-result';
 
-describe('Form validators', () => {
-    let control: FormControl;
-    let group: FormGroup;
-    let invalidResult: InvalidValidationResult;
+describe('form validators', () => {
+  let control: FormControl;
+  let group: FormGroup;
+  let invalidResult: InvalidValidationResult;
 
-    beforeEach(() => {
-        control = new FormControl('');
-        group = new FormGroup({
-            field1: new FormControl(''),
-            field2: new FormControl('')
-        });
+  beforeEach(() => {
+    control = new FormControl('');
+    group = new FormGroup({
+      field1: new FormControl(''),
+      field2: new FormControl('')
     });
+  });
 
-    describe('comparison validator', () => {
-        beforeEach(() => {
-            invalidResult = { invalidComparison : true };
-        });
-
-        it('Should be equal', () => {
-            group.controls['field1'].setValue('hi');
-            group.controls['field2'].setValue('hi');
-            let validator = FormValidators.comparisonValidator('field1', 'field2');
-            expect(validator(group)).toBeNull();
-        })
-
-        it('Should be equal', () => {
-            group.controls['field1'].setValue('I am not equal');
-            group.controls['field2'].setValue('hi');
-            let validator = FormValidators.comparisonValidator('field1', 'field2');
-            expect(validator(group)).toEqual(invalidResult);
-        })
+  describe('email', () => {
+    beforeEach(() => {
+      invalidResult = { invalidEmailAddress: true };
     })
 
-    describe('email validators', () => {
-        beforeEach(() => {
-            invalidResult = { invalidEmailAddress: true };
-        })
-
-        it('Should be valid', () => {
-            control.setValue('iamemail@email.com')
-            expect(FormValidators.emailValidator(control)).toBeNull();
-        });
-
-        it('Should be invalid', () => {
-            control.setValue('i am not an email')
-            expect(FormValidators.emailValidator(control)).toEqual(invalidResult);
-        });
+    it('Should be valid', () => {
+      control.setValue('iamemail@email.com')
+      expect(FormValidators.emailValidator(control)).toBeNull();
     });
 
-})
+    it('Should be invalid', () => {
+      control.setValue('i am not an email')
+      expect(FormValidators.emailValidator(control)).toEqual(invalidResult);
+    });
+  });
+
+  describe('comparison validator', () => {
+    beforeEach(() => {
+      invalidResult = { invalidComparison: true };
+    });
+
+    it('should return null when equal', () => {
+      group.controls['field1'].setValue('hi');
+      group.controls['field2'].setValue('hi');
+      let validator = FormValidators.comparisonValidator('field1', 'field2');
+      expect(validator(group)).toBeNull();
+    });
+
+    it('should return error when not equal', () => {
+      group.controls['field1'].setValue('I am not equal');
+      group.controls['field2'].setValue('hi');
+      let validator = FormValidators.comparisonValidator('field1', 'field2');
+      expect(validator(group)).toEqual(invalidResult);
+    });
+  });
+
+
+// re-testing Angular's Validator wrappers
+  describe('required', () => {
+    it('should error on an empty string',
+      () => { expect(FormValidators.required(new FormControl(''))).toEqual({ 'required': true }); });
+
+    it('should error on null',
+      () => { expect(FormValidators.required(new FormControl(null))).toEqual({ 'required': true }); });
+
+    it('should not error on a non-empty string',
+      () => { expect(FormValidators.required(new FormControl('not empty'))).toBeNull(); });
+
+    it('should accept zero as valid',
+      () => { expect(FormValidators.required(new FormControl(0))).toBeNull(); });
+  })
+
+  describe('requiredTrue', () => {
+    it('should error on false',
+      () => expect(FormValidators.requiredTrue(new FormControl(false))).toEqual({ 'required': true }));
+
+    it('should not error on true',
+      () => expect(FormValidators.requiredTrue(new FormControl(true))).toBeNull());
+  });
+
+  describe('minLength', () => {
+    it('should not error on an empty string',
+      () => { expect(FormValidators.minLength(2)(new FormControl(''))).toBeNull(); });
+
+    it('should not error on null',
+      () => { expect(FormValidators.minLength(2)(new FormControl(null))).toBeNull(); });
+
+    it('should not error on undefined',
+      () => { expect(FormValidators.minLength(2)(new FormControl(null))).toBeNull(); });
+
+    it('should not error on valid strings',
+      () => { expect(FormValidators.minLength(2)(new FormControl('aa'))).toBeNull(); });
+
+    it('should error on short strings', () => {
+      expect(FormValidators.minLength(2)(new FormControl('a'))).toEqual({
+        'minlength': { 'requiredLength': 2, 'actualLength': 1 }
+      });
+    });
+
+    it('should not error when FormArray has valid length', () => {
+      const fa = new FormArray([new FormControl(''), new FormControl('')]);
+      expect(FormValidators.minLength(2)(fa)).toBeNull();
+    });
+
+    it('should error when FormArray has invalid length', () => {
+      const fa = new FormArray([new FormControl('')]);
+      expect(FormValidators.minLength(2)(fa)).toEqual({
+        'minlength': { 'requiredLength': 2, 'actualLength': 1 }
+      });
+    });
+  });
+
+  describe('maxLength', () => {
+    it('should not error on an empty string',
+      () => { expect(FormValidators.maxLength(2)(new FormControl(''))).toBeNull(); });
+
+    it('should not error on null',
+      () => { expect(FormValidators.maxLength(2)(new FormControl(null))).toBeNull(); });
+
+    it('should not error on valid strings',
+      () => { expect(FormValidators.maxLength(2)(new FormControl('aa'))).toBeNull(); });
+
+    it('should error on long strings', () => {
+      expect(FormValidators.maxLength(2)(new FormControl('aaa'))).toEqual({
+        'maxlength': { 'requiredLength': 2, 'actualLength': 3 }
+      });
+    });
+
+    it('should not error when FormArray has valid length', () => {
+      const fa = new FormArray([new FormControl(''), new FormControl('')]);
+      expect(FormValidators.maxLength(2)(fa)).toBeNull();
+    });
+
+    it('should error when FormArray has invalid length', () => {
+      const fa = new FormArray([new FormControl(''), new FormControl('')]);
+      expect(FormValidators.maxLength(1)(fa)).toEqual({
+        'maxlength': { 'requiredLength': 1, 'actualLength': 2 }
+      });
+    });
+  });
+
+  function validator(key: string, error: any) {
+    return function (c: AbstractControl) {
+      const r: { [k: string]: string } = {};
+      r[key] = error;
+      return r;
+    };
+  }
+
+  describe('compose', () => {
+    it('should return null when given null',
+      () => { expect(FormValidators.compose(null)).toBe(null); });
+
+    it('should collect errors from all the validators', () => {
+      const c = FormValidators.compose([validator('a', true), validator('b', true)]);
+      expect(c(new FormControl(''))).toEqual({ 'a': true, 'b': true });
+    });
+
+    it('should run validators left to right', () => {
+      const c = FormValidators.compose([validator('a', 1), validator('a', 2)]);
+      expect(c(new FormControl(''))).toEqual({ 'a': 2 });
+    });
+
+    it('should return null when no errors', () => {
+      const c = FormValidators.compose([FormValidators.nullValidator, FormValidators.nullValidator]);
+      expect(c(new FormControl(''))).toBeNull();
+    });
+
+    it('should ignore nulls', () => {
+      const c = FormValidators.compose([null, FormValidators.required]);
+      expect(c(new FormControl(''))).toEqual({ 'required': true });
+    });
+  });
+
+});
--- src/form-validators.ts
+++ src/form-validators.ts
@@ -1,37 +1,61 @@
 import { FormControl, FormGroup, ValidatorFn, AbstractControl } from '@angular/forms';
 import { InvalidValidationResult } from './invalid-validation-result';
+import { Validators } from '@angular/forms';
 
 export class FormValidators {
-    static emailValidator(control: AbstractControl): InvalidValidationResult {
-        const regex = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
-        return control.value.match(regex) ? null : { invalidEmailAddress: true };
-    }
 
-    static urlValidator(control: AbstractControl): InvalidValidationResult {
-        const regex = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/;;
-        return control.value.match(regex) ? null : { invalidUrl : true };
-    }
+  private static emailValidator(control: AbstractControl): InvalidValidationResult {
+    const regex = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
+    return control.value.match(regex) ? null : { invalidEmailAddress: true };
+  }
 
-    static passwordValidator(control: AbstractControl): InvalidValidationResult  {
-        // {6,100}           - Assert password is between 6 and 100 characters
-        // (?=.*[0-9])       - Assert a string has at least one number
-        const regex = /^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,100}$/;
-        return control.value.match(regex) ? null :  { invalidPassword: true };
-    }
+  private static urlValidator(control: AbstractControl): InvalidValidationResult {
+    const regex = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/;;
+    return control.value.match(regex) ? null : { invalidUrl: true };
+  }
 
-    static numberValidator(control: AbstractControl): InvalidValidationResult {
-        const regex = /^\d/;
-        return control.value.match(regex) ? null : { invalidNumber: true };
-    }
+   private static numberValidator(control: AbstractControl): InvalidValidationResult {
+    const regex = /^\d/;
+    return control.value.match(regex) ? null : { invalidNumber: true };
+  }
 
-    static alphaValidator(control: AbstractControl): InvalidValidationResult {
-        const regex =  /^[A-Za-z]+$/;
-        return control.value.match ? null : { invalidAlphaChar: true };
-    }
+  private static alphaValidator(control: AbstractControl): InvalidValidationResult {
+    const regex = /^[A-Za-z]+$/;
+    return control.value.match ? null : { invalidAlphaChar: true };
+  }
 
-    static comparisonValidator(field1: string, field2: string){
-        return function(group: FormGroup): InvalidValidationResult{
-            return group.controls[field1].value === group.controls[field2].value ? null : { invalidComparison : true };
-        }
-    }
+  private static comparisonValidator(field1: string, field2: string) {
+    return function (group: FormGroup): InvalidValidationResult {
+      return group.controls[field1].value === group.controls[field2].value ? null : { invalidComparison: true };
+    };
+  }
+
+
+  static required(control: AbstractControl): InvalidValidationResult {
+    return Validators.required(control);
+  }
+
+  static requiredTrue(control: AbstractControl): InvalidValidationResult {
+    return Validators.requiredTrue(control);
+  }
+
+  static minLength(minLength: number): ValidatorFn {
+    return Validators.minLength(minLength);
+  }
+
+  static maxLength(maxLength: number): ValidatorFn {
+    return Validators.maxLength(maxLength);
+  }
+
+  static nullValidator(control: AbstractControl): InvalidValidationResult {
+    return Validators.nullValidator(control);
+  }
+
+  static compose(validators: ValidatorFn[]): ValidatorFn {
+    return Validators.compose(validators);
+  }
+
+  static composeAsync(validators: ValidatorFn[]): ValidatorFn {
+    return Validators.composeAsync(validators);
+  }
 }
