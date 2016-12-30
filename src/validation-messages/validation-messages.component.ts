1bc45eea660b704bcd5ef969fc6b295cf17b5c3b
Toxicable <fabianwiles@live.com>
2016-12-30 14:08:38 +1300

    refactored ValidationMessagesComponent to use Observables and be OnPush

--- src/validation-messages/validation-messages.component.ts
+++ src/validation-messages/validation-messages.component.ts
@@ -1,53 +1,50 @@
-import { Component, Input, Inject } from '@angular/core';
+import { Component, Input, Inject, ChangeDetectionStrategy } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ValidationMessagesMapFn } from './validation-messages-map-fn';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
 @Component({
   selector: 'av-validation-messages',
-  template: `<span *ngIf="errorMessage !== null">{{errorMessage}}</span>`
+  template: `<span>{{errorMessages$ | async}}</span>`,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 
 })
 export class ValidationMessagesComponent {
   @Input() control: FormControl;
   @Input() group: FormGroup;
+  errorMessages$: Observable<string>;
+
   constructor(
     @Inject('validationMessageMapper') private mapper: ValidationMessagesMapFn
   ) { }
-  errorMessage$: Observable<string>;
+
   ngOnInit() {
-    // if (this.control) {
-    //   this.errorMessage$ = this.control.valueChanges
-    //     .map(value => {
-    //       for (let propertyName in this.control.errors) {
-    //         if (this.control.errors.hasOwnProperty(propertyName) && this.control.touched) {
-    //           return this.mapper(propertyName, this.control.errors[propertyName]);
-    //         }
-    //       }
-    //       return null;
-    //     })
-    // } else if (this.group) {
-
-    // }
-  }
-  get errorMessage() {
     if (this.control) {
-      for (let propertyName in this.control.errors) {
-        if (this.control.errors.hasOwnProperty(propertyName) && this.control.touched) {
-          return this.mapper(propertyName, this.control.errors[propertyName]);
-        }
-      }
+      this.errorMessages$ = this.control.statusChanges
+        .map((status: string) => {
+          if (this.control.touched && status === 'INVALID') {
+            let keys = Object.keys(this.control.errors);
+            let first = keys.shift();
+            return this.mapper(first, this.control.errors[first]);
+          }
+          return null;
+        });
+      // update so that out observable above emits the inital value
+      this.control.updateValueAndValidity();
 
-      return null;
-    }
-    else if (this.group) {
-      for (let propertyName in this.group.errors) {
-        if (this.group.errors.hasOwnProperty(propertyName) && this.group.touched) {
-          return this.mapper(propertyName, this.group.errors[propertyName]);
-        }
-      }
-
-      return null;
+    } else if (this.group) {
+      this.errorMessages$ = this.group.statusChanges
+        .map((status: string) => {
+          if (this.group.touched && status === 'INVALID') {
+            // since it's invalid we assume that it has at least 1 error in the `this.group.errors` object
+            let errorKeys = Object.keys(this.group.errors);
+            return this.mapper(errorKeys[0], this.group.errors[errorKeys[0]]);
+          }
+          return null;
+        });
+      // update so that out observable above emits the inital value
+      this.group.updateValueAndValidity();
     }
   }
 }
