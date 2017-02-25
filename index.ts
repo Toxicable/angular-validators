a4f9eb3e34b8630a53de71315c5b6a04c558afb8
Toxicable <fabianwiles@live.com>
2017-02-25 18:42:19 +1300

    export validation message mapper token

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { VALIDATION_MESSAGE_MAPPER } from './src/validation-messages/validation-messages-mapper-token';
+export { VALIDATION_MESSAGE_MAPPER } from './src/validation-messages/validation-messages-mapper-token';
 export { FormValidators } from './src/validators/form-validators';
 export { ValidationMessagesComponent } from './src/validation-messages/validation-messages.component';
 export { ValidationMessagesModule } from './src/validation-messages/validation-messages.module';
